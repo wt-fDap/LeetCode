6e0207a97723ac40d5e79bad8ee3f9100925bdc4
Sanjith <sssanjith6905@gmail.com>
2026-08-22 19:24:56 +0530

    Update 3918_check_divisibility_by_digit_sum_and_product.java - LeetCode Problem 3918

--- 3918_check_divisibility_by_digit_sum_and_product.java
+++ 3918_check_divisibility_by_digit_sum_and_product.java
@@ -2,8 +2,6 @@ class Solution {
     public boolean checkDivisibility(int n) {
         int sumofdigits=0;
         int productofdigits=1;
-        int totalsum=0;
-        int length = String.valueOf(Math.abs(n)).length();
         int m=n;
         while(m!=0){
             int temp=m%10;
@@ -12,7 +10,7 @@ class Solution {
             productofdigits*=temp;
             
         }
-        totalsum=totalsum+sumofdigits+productofdigits;
+        int totalsum=sumofdigits+productofdigits;
         return n%totalsum==0;
     }
 }
