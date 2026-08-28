f8f97c0a8eb2e7447879c1657c576c1733b68788
Sanjith <sssanjith6905@gmail.com>
2026-08-28 09:53:40 +0530

    Add 898_transpose_matrix.java - LeetCode Problem 898

---
+++ 898_transpose_matrix.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Solution {
+    public int[][] transpose(int[][] matrix) {
+        int n=matrix.length;
+        int m=matrix[0].length;
+        int [][] mat= new int[m][n];
+        for(int i=0; i<m; i++){
+            for(int j=0; j<n; j++){
+                mat[i][j]=matrix[j][i];
+            }
+        }
+        return mat;
+    }
+}
