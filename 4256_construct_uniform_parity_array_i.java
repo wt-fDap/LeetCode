f8d45c2ef4995384567959a462af74b6370af54b
Sanjith <sssanjith6905@gmail.com>
2026-09-02 16:31:30 +0530

    Add 4256_construct_uniform_parity_array_i.java - LeetCode Problem 4256

---
+++ 4256_construct_uniform_parity_array_i.java
@@ -0,0 +1,22 @@
+// class Solution {
+//     public boolean uniformArray(int[] nums1) {
+//         n=nums1.length;
+//         int odd=0; int even=0;
+//         int [] nums2= new int[n];
+//         for(int i=0; i<n; i++){
+//             if(nums[i]%2==0) even++;
+//             else odd++;
+//             }
+//         }
+//         if(even==n || odd==n) return true;
+//         if(even!=n){
+            
+//         }
+//     }
+// }
+
+class Solution {
+     public boolean uniformArray(int[] nums1) {
+        return true;
+     }
+}
